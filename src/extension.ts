36a0c3c6e6da4c613db50f095886fb38c77a9022
thien2001git <hoangthien66771508@gmail.com>
2024-06-15 01:16:07 +0700

    init

---
+++ src/extension.ts
@@ -0,0 +1,51 @@
+import * as vscode from 'vscode';
+const fs = require('fs');
+
+export function activate(context: vscode.ExtensionContext) {
+
+	console.log('Congratulations, your extension "myextension" is now active!');
+
+	const disposable = vscode.commands.registerCommand('list-files', () => {
+
+		function createFile(fileName: String, content: string) {
+			const wsedit = new vscode.WorkspaceEdit();
+			if (vscode.workspace.workspaceFolders !== undefined) {
+				const wsPath = vscode.workspace.workspaceFolders[0].uri.fsPath; // gets the path of the first workspace folder
+				const filePath = vscode.Uri.file(wsPath + `/${fileName}`);
+				vscode.window.showInformationMessage(filePath.toString());
+				wsedit.createFile(filePath, { ignoreIfExists: true });
+				wsedit.insert(filePath, new vscode.Position(0, 0), content);
+				vscode.workspace.applyEdit(wsedit);
+				vscode.window.showInformationMessage(`Created a new file: ${fileName}`);
+			}
+		}
+
+		const options: vscode.OpenDialogOptions = {
+			canSelectMany: false,
+			openLabel: 'Select',
+			canSelectFiles: false,
+			canSelectFolders: true
+		};
+
+		vscode.window.showOpenDialog(options).then(fileUri => {
+			if (fileUri && fileUri[0]) {
+				const folder = fileUri[0].fsPath;
+				console.log('Selected file: ' + fileUri[0].fsPath);
+
+				fs.readdir(folder, (err: String, files: Array<String>) => {
+					var filesStr = "";
+					files.forEach(file => {
+						filesStr += `${file}\n`;
+					});
+
+					createFile("name.txt", filesStr);
+				});
+			}
+		});
+	});
+
+	context.subscriptions.push(disposable);
+}
+
+// This method is called when your extension is deactivated
+export function deactivate() { }
